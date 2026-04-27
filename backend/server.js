d431e80ccafba7a44d8bf996ff126fadac0ae888
Rudra Gajera <gajerarudra96@gmail.com>
2026-04-27 15:39:52 +0530

    Fix render port config

--- backend/server.js
+++ backend/server.js
@@ -41,6 +41,8 @@ app.use("/api/upload", uploadRoutes);
 app.use("/api/stripe", stripeRoutes);
 
 // Start server
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT=process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(` Server running on port ${PORT}`);
 });
